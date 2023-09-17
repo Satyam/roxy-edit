cabccab87950410e09a0e11eff7bf48a1725cebd
Satyam <satyam@satyam.com.ar>
2023-09-17 19:32:01 +0200

    Create bundle for both client and server. Server bundle now includes `node_module` code. Copied over `hexo` folder from previous incarnation.  Added it to gitignore

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,23 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
-export default {
-  input: 'src/client/main.js',
-  output: {
-    file: 'public/main.js',
-    format: 'cjs',
+export default [
+  {
+    input: 'src/client/main.js',
+    output: {
+      file: 'public/main.js',
+      format: 'cjs',
+    },
+    plugins: [nodeResolve(), commonjs()],
   },
-  plugins: [nodeResolve(), commonjs()],
-};
+  {
+    input: 'src/server/index.js',
+    output: {
+      file: 'server/index.js',
+      format: 'es',
+    },
+    plugins: [nodeResolve(), commonjs(), json()],
+    // external: [/node_modules/],
+  },
+];
