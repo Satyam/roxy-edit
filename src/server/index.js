ea2b38378670682b8b92a1d42e100097e2419b86
Satyam <satyam@satyam.com.ar>
2023-09-21 17:41:25 +0200

    Added a bunch of routes to the server.

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import open from 'open';
 
-import { loadInfo } from './data.js';
 import imgRouter from './images.js';
+import menuRouter from './menu.js';
+import filesRouter from './files.js';
+import infoRouter from './data.js';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/node_modules', express.static('node_modules'));
 
 app.use('/images', imgRouter);
+app.use('/files', filesRouter);
+app.use('/images', menuRouter);
+app.use('/info', infoRouter);
+app.use('/menu', menuRouter);
 
 app.get('/exit/:code?', (req, res) => {
   res.end('gone');
@@ -21,6 +28,5 @@ app.get('/exit/:code?', (req, res) => {
 
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`);
-  await loadInfo();
   await open(`http://localhost:${port}`);
 });
