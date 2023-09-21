60521a11a74ae21d2b3fb5b1670cb6fa720cb404
Satyam <satyam@satyam.com.ar>
2023-09-21 17:43:16 +0200

    Dropped a bunch of code from `data.js` on the server side, since most of the work really happens in the client anyway.

--- src/server/data.js
+++ src/server/data.js
@@ -1,6 +1,7 @@
+import express from 'express';
 import { join } from 'node:path';
 import { readJson, writeJson } from './utils.js';
-import { fileName, isPost, isNew } from './state';
+import { fileName, isPost, isNew } from '../common/state.js';
 
 export const HEXO_DIR = 'hexo/';
 export const DOCUMENT_ROOT = 'server/';
@@ -10,132 +11,42 @@ export const SRC_PAGES_DIR = join(HEXO_DIR, 'source/');
 export const DELETED_PAGS_DIR = join(SRC_PAGES_DIR, '_deleted');
 export const HEXO_IMG_DIR = join(SRC_PAGES_DIR, IMG_DIR);
 export const DRAFTS_DIR = join(DOCUMENT_ROOT, 'drafts/');
-const DRAFTS_INFO = join(DRAFTS_DIR, 'info.json');
+const INFO_JSON = join(DOCUMENT_ROOT, 'info.json');
 export const EDITOR_IMG_DIR = join(DOCUMENT_ROOT, IMG_DIR);
 export const MENU_CONFIG = join(HEXO_DIR, '_config.roxy.yml');
 
 let info;
 
-export const getCategories = () => info.categories;
-export const getTags = () => info.tags;
-export const getAuthors = () => info.authors;
-export const getPages = () => info.pages;
-export const getPosts = () => info.posts;
-export const getDrafts = (post = false) =>
-  info.drafts.filter((p) => p.isPost === post);
+export const infoRouter = new express.Router();
+export default infoRouter;
+infoRouter.get('/', async (_, res) => {
+  if (!info) {
+    try {
+      info = Object.assign(
+        {
+          pages: [],
+          posts: [],
+          categories: [],
+          tags: [],
+          authors: [],
+          drafts: [],
+        },
+        await readJson(INFO_JSON)
+      );
+    } catch (err) {
+      res.status(500).send(err);
+    }
+    ['categories', 'tags', 'authors'].forEach((sel) => {
+      info[sel].sort();
+    });
+  }
+  res.send(info);
+});
 
-export const saveInfo = () => writeJson(DRAFTS_INFO, info);
-
-export const removeDraftInfo = async () => {
-  info.drafts = info.drafts.filter(
-    (data) => !(data.file === fileName && data.isPost === isPost)
+infoRouter.post('/', (req, res) => {
+  info = req.body;
+  writeJson(INFO_JSON, info).then(
+    () => res.status(200).end(),
+    (err) => res.status(500).send(err)
   );
-  await saveInfo();
-};
-
-export const removePostInfo = async () => {
-  if (isPost) {
-    info.posts = info.posts.filter((data) => data.file !== fileName);
-  } else {
-    info.pages = info.pages.filter((data) => data.file !== fileName);
-  }
-  await saveInfo();
-};
-
-export const addPostInfo = async (fileInfo) => {
-  if (isPost) {
-    info.posts.push(fileInfo);
-  } else {
-    info.pages.push(fileInfo);
-  }
-  await saveInfo();
-};
-
-export const addDraftInfo = async (fileInfo) => {
-  const newInfo = {
-    ...fileInfo,
-    file: fileName,
-    isPost,
-    isNew,
-  };
-  if (
-    !info.drafts.some((data, index) => {
-      if (data.file === fileName && data.isPost === isPost) {
-        info.drafts[index] = newInfo;
-        return true;
-      }
-    })
-  ) {
-    info.drafts.push(newInfo);
-  }
-  await saveInfo();
-};
-
-export const updateProps = async (matter) => {
-  const { categories, tags, author } = matter;
-  let changed = false;
-  categories.forEach((cat) => {
-    if (!info.categories.includes(cat)) {
-      info.categories.push(cat);
-      info.categories.sort();
-      changed = true;
-    }
-  });
-  tags.forEach((tag) => {
-    if (!info.tags.includes(tag)) {
-      info.tags.push(tag);
-      info.tags.sort();
-      changed = true;
-    }
-  });
-  if (!info.authors.includes(author)) {
-    info.authors.push(author);
-    info.authors.sort();
-    changed = true;
-  }
-
-  if (changed) {
-    await saveInfo();
-  }
-  return changed;
-};
-
-export const uniqueFileName = (fName) => {
-  let newName;
-  for (let count = 0, found = false; !found; count++) {
-    newName = count ? `${fName}-${count}.md` : `${fName}.md`;
-    if (isPost) {
-      found = info.posts.every((data) => data.file !== newName);
-    } else {
-      found = info.pages.every((data) => data.file !== newName);
-    }
-    found = found && info.drafts.every((data) => data.file !== newName);
-  }
-  return newName;
-};
-
-const INFO_PROPS = [
-  'pages',
-  'posts',
-  'categories',
-  'tags',
-  'authors',
-  'drafts',
-];
-
-export const loadInfo = async () => {
-  info = Object.assign(
-    {
-      pages: [],
-      posts: [],
-      categories: [],
-      tags: [],
-      authors: [],
-      drafts: [],
-    },
-    await readJson(DRAFTS_INFO)
-  );
-  ['categories', 'tags', 'authors'].forEach((sel) => {
-    info[sel].sort();
-  });
-};
+});
