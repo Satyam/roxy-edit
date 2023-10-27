1021d78ff1d640dff62f2edc34aba634e079d8da
Satyam <satyam@satyam.com.ar>
2023-10-27 17:23:11 +0200

    Added DataContext and DataProvider to handle  shared storage of `info.json`

---
+++ src/solid/DataContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext } from 'solid-js';
+import { createStore } from 'solid-js/store';
+import { fetchJson, sendJson } from '../fetch';
+import { ROUTES } from '../common';
+
+export const DataContext = createContext();
+
+export function DataProvider(props) {
+  const [data, setStoredData] = createStore({
+    pages: [],
+    posts: [],
+    categories: [],
+    tags: [],
+    authors: [],
+    drafts: [],
+  });
+
+  const providerValue = [
+    data,
+    async (newData) => {
+      setStoredData(newData);
+      await sendJson(ROUTES.INFO, data);
+    },
+  ];
+  fetchJson(ROUTES.INFO).then(setStoredData);
+  return (
+    <DataContext.Provider value={providerValue}>
+      {props.children}
+    </DataContext.Provider>
+  );
+}
+
+export const useData = () => useContext(DataContext);
