489bbbec5e8923ad2b148c6ec105b2b2a5520a20
Satyam <satyam@satyam.com.ar>
2023-11-12 19:12:12 +0100

    Added an enum of button actions to `Select` component

--- src/solid/Select.jsx
+++ src/solid/Select.jsx
@@ -2,6 +2,15 @@ import { createSignal, Switch, Match } from 'solid-js';
 import ListPages from './ListPages';
 import Icon from './Icon';
 
+const BUTTONS = {
+  NEW_PAGE: 'newPage',
+  EDIT_PAGE: 'editPage',
+  DRAFT_PAGE: 'draftPage',
+  NEW_POST: 'newPost',
+  EDIT_POST: 'editPost',
+  DRAFT_POST: 'draftPost',
+};
+
 function Select() {
   const [selection, setSelection] = createSignal('');
 
@@ -10,77 +19,84 @@ function Select() {
   };
 
   return (
-    <section id="select">
-      <div class="two-grid">
-        <div id="selectButtons">
-          <fieldset>
-            <legend>Página</legend>
-            <button
-              name="newPage"
-              onclick={clickHandler}
-              disabled={selection() === 'newPage'}
-            >
-              <Icon>new-doc</Icon>
-              Nueva
-            </button>
-            <button
-              name="editPage"
-              onclick={clickHandler}
-              disabled={selection() === 'editPage'}
-            >
-              <Icon>edit-doc</Icon>
-              Modificar
-            </button>
-            <button
-              name="draftPage"
-              onclick={clickHandler}
-              disabled={selection() === 'draftPage'}
-            >
-              <Icon>draft-doc</Icon>
-              Borradores
-            </button>
-          </fieldset>
-          <fieldset>
-            <legend>Post</legend>
-            <button
-              name="newPost"
-              onclick={clickHandler}
-              disabled={selection() === 'newPost'}
-            >
-              <Icon>new-doc</Icon>
-              Nuevo
-            </button>
-            <button
-              name="editPost"
-              onclick={clickHandler}
-              disabled={selection() === 'editPost'}
-            >
-              <Icon>edit-doc</Icon>
-              Modificar
-            </button>
-            <button
-              name="draftPost"
-              onclick={clickHandler}
-              disabled={selection() === 'draftPost'}
-            >
-              <Icon>draft-doc</Icon>
-              Borradores
-            </button>
-          </fieldset>
-        </div>
-        <Switch fallback={<div>Seleccione alguna opción de la izquierda</div>}>
-          <Match when={selection() === 'newPage'}>aquí va nueva página</Match>
-          <Match when={selection() === 'editPage'}>
-            <ListPages />
-          </Match>
-
-          <Match when={selection() === 'draftPage'}>Estamos en draftPage</Match>
-          <Match when={selection() === 'newPost'}>Estamos en newPost</Match>
-          <Match when={selection() === 'editPost'}>Estamos en editPost</Match>
-          <Match when={selection() === 'draftPost'}>Estamos en draftPost</Match>
-        </Switch>
+    <div class="two-grid">
+      <div id="selectButtons">
+        <fieldset>
+          <legend>Página</legend>
+          <button
+            name={BUTTONS.NEW_PAGE}
+            onclick={clickHandler}
+            disabled={selection() === BUTTONS.NEW_PAGE}
+          >
+            <Icon>new-doc</Icon>
+            Nueva
+          </button>
+          <button
+            name={BUTTONS.EDIT_PAGE}
+            onclick={clickHandler}
+            disabled={selection() === BUTTONS.EDIT_PAGE}
+          >
+            <Icon>edit-doc</Icon>
+            Modificar
+          </button>
+          <button
+            name={BUTTONS.DRAFT_PAGE}
+            onclick={clickHandler}
+            disabled={selection() === BUTTONS.DRAFT_PAGE}
+          >
+            <Icon>draft-doc</Icon>
+            Borradores
+          </button>
+        </fieldset>
+        <fieldset>
+          <legend>Post</legend>
+          <button
+            name={BUTTONS.NEW_POST}
+            onclick={clickHandler}
+            disabled={selection() === BUTTONS.NEW_POST}
+          >
+            <Icon>new-doc</Icon>
+            Nuevo
+          </button>
+          <button
+            name={BUTTONS.EDIT_POST}
+            onclick={clickHandler}
+            disabled={selection() === BUTTONS.EDIT_POST}
+          >
+            <Icon>edit-doc</Icon>
+            Modificar
+          </button>
+          <button
+            name={BUTTONS.DRAFT_POST}
+            onclick={clickHandler}
+            disabled={selection() === BUTTONS.DRAFT_POST}
+          >
+            <Icon>draft-doc</Icon>
+            Borradores
+          </button>
+        </fieldset>
       </div>
-    </section>
+      <Switch fallback={<div>Seleccione alguna opción de la izquierda</div>}>
+        <Match when={selection() === BUTTONS.NEW_PAGE}>
+          aquí va nueva página
+        </Match>
+        <Match when={selection() === BUTTONS.EDIT_PAGE}>
+          <ListPages />
+        </Match>
+        <Match when={selection() === BUTTONS.DRAFT_PAGE}>
+          Estamos en draftPage
+        </Match>
+        <Match when={selection() === BUTTONS.NEW_POST}>
+          Estamos en newPost
+        </Match>
+        <Match when={selection() === BUTTONS.EDIT_POST}>
+          Estamos en editPost
+        </Match>
+        <Match when={selection() === BUTTONS.DRAFT_POST}>
+          Estamos en draftPost
+        </Match>
+      </Switch>
+    </div>
   );
 }
 
