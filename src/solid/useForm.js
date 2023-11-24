385063debf8d5be0b8b34712754e058d349c1d21
Satyam <satyam@satyam.com.ar>
2023-11-24 16:59:53 +0100

    Changed the way of reading the form fields. Instead of adding code to deal with each one, I create a FormData object, which I later convert to a plain Object. To allow for Solid Componets, which FormData ignores, I add a couple of `data-*` attributes to process those. These attributes are commented in the source.

--- src/solid/useForm.js
+++ src/solid/useForm.js
@@ -55,27 +55,56 @@ export function useForm({ errorClass }) {
         }
       }
       if (!errored) {
-        callback(
-          Array.from(form.elements)
-            .filter((el) => el.name)
-            .reduce((vals, el) => {
-              switch (el.nodeName) {
-                case 'SELECT':
-                  return {
-                    ...vals,
-                    [el.name]: Array.from(el.selectedOptions).map(
-                      (el) => el.value
-                    ),
-                  };
-                case 'BUTTON':
-                  return vals;
-                default:
-                  return { ...vals, [el.name]: el.value };
-              }
-            }, {}),
-          e.submitter,
-          ref
-        );
+        const data = {};
+        /*
+        Data is read from the form through a `FormData` instance.
+        Since that might not work with some Solid Components, 
+        there are some options through `data-*` attributes:
+
+        * `data-name`: much like `name` in any other input element
+        * `data-value`: identifies the element that contains the value.
+        * 
+        If the `data-value` is in the same element, it produces a single value.
+        If the `data-value` attr is in child elements, it produces an array of values.
+        
+        Further attributes:
+          * `data-in-value` the values is in the `value` property of the element.
+          * `data-in-html` the value is in the `innerHTML` property of the element.
+          If none of this is present, the value of the `data-value` attribute itself is the value.
+
+         */
+        const setValue = (key, value) => {
+          if (key in data) {
+            if (!Array.isArray(data[key])) data[key] = [data[key]];
+            data[key].push(value);
+          } else {
+            data[key] = value;
+          }
+        };
+
+        for (const [key, value] of new FormData(form)) {
+          setValue(key, value);
+        }
+
+        const getValueFromEl = (el) => {
+          if ('inValue' in el.dataset) return el.value;
+          if ('inHtml' in el.dataset) return el.innerHTML;
+          return el.dataset.value;
+        };
+        form.querySelectorAll('[data-name]').forEach((dataEl) => {
+          const name = dataEl.dataset.name;
+          if ('value' in dataEl.dataset) {
+            setValue(name, getValueFromEl(dataEl));
+          } else {
+            setValue(
+              name,
+              Array.from(dataEl.querySelectorAll('[data-value]')).map(
+                getValueFromEl
+              )
+            );
+          }
+        });
+        callback(data, e.submitter, ref);
       }
     };
   };
