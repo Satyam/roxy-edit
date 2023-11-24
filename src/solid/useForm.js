82e9961c2b1fc17288d3abb59b868b1f53c7dea0
Satyam <satyam@satyam.com.ar>
2023-11-24 17:24:21 +0100

    Fixed: I was reading the actual form simply because it had an id of "form". For a more permanent fix, I gave all `ref` parameters decent names, `formEl` and `fieldEl`. Also separated the code for `getFormValues` function  from inside the `formSubmit` function.

--- src/solid/useForm.js
+++ src/solid/useForm.js
@@ -26,23 +26,76 @@ export function useForm({ errorClass }) {
   const [errors, setErrors] = createStore({}),
     fields = {};
 
-  const validate = (ref, accessor) => {
+  const validate = (fieldEl, accessor) => {
     const accessorValue = accessor();
     const validators = Array.isArray(accessorValue) ? accessorValue : [];
     let config;
-    fields[ref.name] = config = { element: ref, validators };
-    ref.onblur = checkValid(config, setErrors, errorClass);
-    ref.oninput = () => {
-      if (!errors[ref.name]) return;
-      setErrors({ [ref.name]: undefined });
-      errorClass && ref.classList.toggle(errorClass, false);
+    fields[fieldEl.name] = config = { element: fieldEl, validators };
+    fieldEl.onblur = checkValid(config, setErrors, errorClass);
+    fieldEl.oninput = () => {
+      if (!errors[fieldEl.name]) return;
+      setErrors({ [fieldEl.name]: undefined });
+      errorClass && fieldEl.classList.toggle(errorClass, false);
     };
   };
 
-  const formSubmit = (ref, accessor) => {
-    const callback = accessor() || (() => {});
-    ref.setAttribute('novalidate', '');
-    ref.onsubmit = async (e) => {
+  const getFormValues = (formEl) => {
+    const data = {};
+    /*
+    Data is read from the form through a `FormData` instance.
+    Since that might not work with some Solid Components, 
+    there are some options through `data-*` attributes:
+
+    * `data-name`: much like `name` in any other input element
+    * `data-value`: identifies the element that contains the value.
+    * 
+    If the `data-value` is in the same element, it produces a single value.
+    If the `data-value` attr is in child elements, it produces an array of values.
+    
+    Further attributes:
+      * `data-in-value` the values is in the `value` property of the element.
+      * `data-in-html` the value is in the `innerHTML` property of the element.
+      If none of this is present, the value of the `data-value` attribute itself is the value.
+
+     */
+    const setValue = (key, value) => {
+      if (key in data) {
+        if (!Array.isArray(data[key])) data[key] = [data[key]];
+        data[key].push(value);
+      } else {
+        data[key] = value;
+      }
+    };
+
+    for (const [key, value] of new FormData(formEl)) {
+      setValue(key, value);
+    }
+
+    const getValueFromEl = (el) => {
+      if ('inValue' in el.dataset) return el.value;
+      if ('inHtml' in el.dataset) return el.innerHTML;
+      return el.dataset.value;
+    };
+    formEl.querySelectorAll('[data-name]').forEach((dataEl) => {
+      const name = dataEl.dataset.name;
+      if ('value' in dataEl.dataset) {
+        setValue(name, getValueFromEl(dataEl));
+      } else {
+        setValue(
+          name,
+          Array.from(dataEl.querySelectorAll('[data-value]')).map(
+            getValueFromEl
+          )
+        );
+      }
+    });
+    return data;
+  };
+
+  const formSubmit = (formEl, accessor) => {
+    const submitHandler = accessor() || (() => {});
+    formEl.setAttribute('novalidate', '');
+    formEl.onsubmit = async (e) => {
       e.preventDefault();
       let errored = false;
 
@@ -55,61 +108,12 @@ export function useForm({ errorClass }) {
         }
       }
       if (!errored) {
-        const data = {};
-        /*
-        Data is read from the form through a `FormData` instance.
-        Since that might not work with some Solid Components, 
-        there are some options through `data-*` attributes:
-
-        * `data-name`: much like `name` in any other input element
-        * `data-value`: identifies the element that contains the value.
-        * 
-        If the `data-value` is in the same element, it produces a single value.
-        If the `data-value` attr is in child elements, it produces an array of values.
-        
-        Further attributes:
-          * `data-in-value` the values is in the `value` property of the element.
-          * `data-in-html` the value is in the `innerHTML` property of the element.
-          If none of this is present, the value of the `data-value` attribute itself is the value.
-
-         */
-        const setValue = (key, value) => {
-          if (key in data) {
-            if (!Array.isArray(data[key])) data[key] = [data[key]];
-            data[key].push(value);
-          } else {
-            data[key] = value;
-          }
-        };
-
-        for (const [key, value] of new FormData(form)) {
-          setValue(key, value);
-        }
-
-        const getValueFromEl = (el) => {
-          if ('inValue' in el.dataset) return el.value;
-          if ('inHtml' in el.dataset) return el.innerHTML;
-          return el.dataset.value;
-        };
-        form.querySelectorAll('[data-name]').forEach((dataEl) => {
-          const name = dataEl.dataset.name;
-          if ('value' in dataEl.dataset) {
-            setValue(name, getValueFromEl(dataEl));
-          } else {
-            setValue(
-              name,
-              Array.from(dataEl.querySelectorAll('[data-value]')).map(
-                getValueFromEl
-              )
-            );
-          }
-        });
-        callback(data, e.submitter, ref);
+        submitHandler(getFormValues(formEl), e.submitter, formEl);
       }
     };
   };
 
-  return { validate, formSubmit, errors };
+  return { validate, formSubmit, errors, getFormValues };
 }
 
 export default useForm;
